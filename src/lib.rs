bf5377e1409bab5c6c6432ad6c1ddf60f11efe21
Centril <twingoow@gmail.com>
2015-02-05 15:09:38 +0100

    unsafe_destructor (Drop needs this...)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 #![crate_type = "lib"]
 #![crate_name = "fsnotify"]
 
+// https://github.com/rust-lang/rust/issues/17858
+#![feature(unsafe_destructor)]
+
 // Required by operations.rs
 #![feature(hash)]
 #[macro_use] extern crate bitflags;
 
 mod fsnotify;
 pub use self::fsnotify::*;
+
+#[cfg(target_os="linux")]
+pub mod linux;
+
+#[cfg(target_os="osx")]
+pub mod osx;
+
+#[cfg(target_os="windows")]
+pub mod win;
